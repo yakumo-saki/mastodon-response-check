8f0921ede75bafb066644d4472c4c5b539a331b8
Yakumo Saki <yakumo@ziomatrix.org>
2019-05-27 16:50:50 +0900

    add readme

--- README.md
+++ README.md
@@ -14,15 +14,18 @@ npm install
 set environment value below
 
 * login_url
-mastodon login url. ( https://mastodon.example.com/auth/sign_in )
+mastodon login url. ( example: https://mastodon.example.com/auth/sign_in )
 
 * USERNAME
 * PASSWORD
 
 mastodon username and password.
 
-### example
+### commandline example
 
 ```
-
+USERNAME=<mail addr>; PASSWORD=<your password>; LOGIN_URL=http://mastodon.example.com/auth/sign_in node index.js
 ```
+
+Response time (ms) output to stdout.
+All other messages are output to stderr.
--- index.js
+++ index.js
@@ -1,30 +1,63 @@
 const puppeteer = require('puppeteer');
 
+const login_url = process.env.LOGIN_URL;
 const username = process.env.USERNAME;
 const password = process.env.PASSWORD;
-
 const DEBUG = process.env.DEBUG == true;
+
+// console.log(username, password, DEBUG);
+
 
 (async () => {
   const browser = await puppeteer.launch();
+
+  const start_ms = new Date().getTime();
+
   const page = await browser.newPage();
-  await page.setViewport({width: 1024, height: 768});
+  page.setViewport({width: 1024, height: 768});
 
-  await page.goto('https://maoh.company/auth/sign_in', {waitUntil: "domcontentloaded"});
-  
-  await page.screenshot({path: 'screenshots/login1.png'});
+  var exec = require('child_process').exec;
+  await exec('rm ./screenshots/*.png');
 
-  await page.type('#user_email', username);
-  await page.type('#user_password', password);
+  try {
+    await page.goto(login_url, {waitUntil: "networkidle2"});
+    
+    await page.screenshot({path: './screenshots/login1.png'});
 
-  await page.screenshot({path: 'screenshots/login2.png'});
+    await page.type('#user_email', username);
+    await page.type('#user_password', password);
 
-  await Promise.all([
-    page.click("button[type=submit]"),
-    page.waitForNavigation({ waitUntil: 'networkidle2' })
-  ]);
+    await page.screenshot({path: './screenshots/finish1.png'});
+    await page.click("button[type=submit]", {waitUntil: "networkidle2"});
 
-  await page.screenshot({path: 'screenshots/finish.png'});
+    await new Promise(r => setTimeout(r, 500));
 
-  await browser.close();
+    var ss_count = 1;
+    var loaded = false;
+    while (loaded == false) {
+      console.error("wait loop " + ss_count);
+
+      await page.screenshot({path: './screenshots/wait' + ss_count + '.png'});
+      if (await page.$("div.status") !== null) {
+        console.error("found!");
+        loaded = true;
+      }
+
+      ss_count++;
+
+      if (ss_count > 30) {
+        console.error("give up.");
+        loaded = true;
+      } else {
+        await new Promise(r => setTimeout(r, 500));
+      }
+    }
+
+    const end_ms = new Date().getTime() - start_ms;
+    console.log(end_ms);
+    console.error("response time is " + end_ms + "ms");
+  } finally {
+    await browser.close();
+  }
+
 })();
